3893f9958833026cd23f7971782077ff5d4b38af
bkoatz <bkoatz@nyc.rr.com>
2013-03-29 18:15:19 -0300

    Basic Perks static class

---
+++ src/edu/brown/cs32/browndemic/disease/Perks.java
@@ -0,0 +1,12 @@
+package disease;
+import java.util.ArrayList;
+
+/**
+ *@author bkoatz 
+ */
+public static class Perks{
+ 
+  final Perk VOMITING = new Perk(2, 0, 2.0, 6.0,
+              new ArrayList<Perk>());
+  
+}
