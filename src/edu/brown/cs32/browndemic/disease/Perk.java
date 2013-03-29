9eeb76d09fb0658a9f22f61646a933430e6be0d5
bkoatz <bkoatz@nyc.rr.com>
2013-03-29 18:14:36 -0300

    Better Perk class

--- src/edu/brown/cs32/browndemic/disease/Perk.java
+++ src/edu/brown/cs32/browndemic/disease/Perk.java
@@ -21,11 +21,37 @@ public class Perk{
   //the boolean availability of this perk to be purchased
   private boolean _availability = false;
   
+  //the boolean saying whether or not this perk has been purchased
+  private boolean _bought = false;
+  
   //the ArrayList of perks that this perk's purchase makes available
   private ArrayList<Perk> _nextPerks;
   
   //constuctor: sets cost, change in infectivity/lethality/visibility and
-  //
-  public Perk(
+  //the perks that, when this perk is bought, become available for purchase
+  public Perk(int tempcost, double tempinf, double templeth, double tempvis,
+              ArrayList<Perk> tempnext){
+    
+    this._cost = tempcost;
+    this._infectivityChange = tempinf;
+    this._lethalityChange = templeth;
+    this._visibilityChange = tempvis;
+    this._nextPerks = tempnext;
+    
+  }
+  
+  //sets the availability of of this perk
+  public void setAvailability(boolean avail){
+    
+    this._availability = avail;
+    
+  }
+  
+  //sets whether this perk has been bought
+  public void setBought(boolean bought){
+    
+    this._bought = bought;
+    
+  }
   
 }
