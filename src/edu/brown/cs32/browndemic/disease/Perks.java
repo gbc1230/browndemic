86f51d004116af42081a36e4f80b244825603d23
bkoatz <bkoatz@nyc.rr.com>
2013-03-30 14:14:37 -0300

    Basic Perks framework with VIRUS and BACTERIA perks

--- src/edu/brown/cs32/browndemic/disease/Perks.java
+++ src/edu/brown/cs32/browndemic/disease/Perks.java
@@ -1,12 +1,15 @@
-package disease;
+package edu.brown.cs32.browndemic.disease;
 import java.util.ArrayList;
 
 /**
  *@author bkoatz 
  */
-public class Perks{
+public interface Perks{
  
-  final Perk VOMITING = new Perk(2, 0, 2.0, 6.0, 0, 0, 0, 0, 0,
+  final Perk VOMITING_VIRUS = new Perk(2, 3, 0, 2.0, 6.0, 0, 0, 0, 0, 0,
+              new ArrayList<Perk>());
+  
+  final Perk VOMITING_BACTERIA = new Perk(2, 1, 0, 2.0, 6.0, 0, 0, 0, 0, 0,
               new ArrayList<Perk>());
   
 }
