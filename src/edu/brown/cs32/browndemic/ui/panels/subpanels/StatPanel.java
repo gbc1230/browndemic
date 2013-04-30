baa1bb660e8e1afbd9fcd6ea12179d2d96307c11
Ben Most <bmost1022@gmail.com>
2013-04-29 23:42:46 -0400

    More stats!

--- src/edu/brown/cs32/browndemic/ui/panels/subpanels/StatPanel.java
+++ src/edu/brown/cs32/browndemic/ui/panels/subpanels/StatPanel.java
@@ -23,7 +23,7 @@ public class StatPanel extends BrowndemicPanel {
 	private static final long serialVersionUID = -1147093041368550992L;
 	
 	private Disease _disease;
-	private JProgressBar _infectivity, _lethality, _visibility;
+	private JProgressBar _infectivity, _lethality, _visibility, _heat, _cold, _wet, _dry, _medicine;
 
 	public StatPanel(Disease d) {
 		super();
@@ -35,6 +35,11 @@ public class StatPanel extends BrowndemicPanel {
 				_infectivity.setValue((int)_disease.getInfectivity());
 				_lethality.setValue((int)_disease.getLethality());
 				_visibility.setValue((int)_disease.getVisibility());
+				_heat.setValue((int)_disease.getHeatRes());
+				_cold.setValue((int)_disease.getColdRes());
+				_wet.setValue((int)_disease.getWetRes());
+				_dry.setValue((int)_disease.getDryRes());
+				_medicine.setValue((int)_disease.getMedRes());
 			}
 		}).start();
 	}
@@ -44,68 +49,61 @@ public class StatPanel extends BrowndemicPanel {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBorder(BorderFactory.createLineBorder(Colors.RED_TEXT, 2));
 		
-		JPanel infectivity = new JPanel();
-		infectivity.setLayout(new BoxLayout(infectivity, BoxLayout.X_AXIS));
-		infectivity.setBackground(Colors.MENU_BACKGROUND);
-		infectivity.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-		
-		JLabel infectivityLabel = new JLabel("Infectivity: ");
-		infectivityLabel.setForeground(Colors.RED_TEXT);
-		infectivityLabel.setFont(Fonts.NORMAL_TEXT);
 		
 		_infectivity = new JProgressBar(0, (int)_disease.getMaxInfectivity());
-		_infectivity.setValue((int) _disease.getInfectivity());
-		_infectivity.setBackground(Colors.MENU_BACKGROUND);
-		_infectivity.setForeground(Colors.RED_TEXT);
-		//_infectivity.setBorder(BorderFactory.createLineBorder(Colors.RED_TEXT, 1));
-		
-		infectivity.add(infectivityLabel);
-		infectivity.add(_infectivity);
-		
-		
-		JPanel lethality = new JPanel();
-		lethality.setLayout(new BoxLayout(lethality, BoxLayout.X_AXIS));
-		lethality.setBackground(Colors.MENU_BACKGROUND);
-		lethality.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-		
-		JLabel lethalityLabel = new JLabel("Lethality: ");
-		lethalityLabel.setForeground(Colors.RED_TEXT);
-		lethalityLabel.setFont(Fonts.NORMAL_TEXT);
+		JPanel infectivity = createPanel("Infectivity: ", _infectivity);
 		
 		_lethality = new JProgressBar(0, (int)_disease.getMaxLethality());
-		_lethality.setValue((int) _disease.getLethality());
-		_lethality.setBackground(Colors.MENU_BACKGROUND);
-		_lethality.setForeground(Colors.RED_TEXT);
-		
-		lethality.add(lethalityLabel);
-		lethality.add(_lethality);
-		
-		
-		JPanel visibility = new JPanel();
-		visibility.setLayout(new BoxLayout(visibility, BoxLayout.X_AXIS));
-		visibility.setBackground(Colors.MENU_BACKGROUND);
-		visibility.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-		
-		JLabel visibilityLabel = new JLabel("Visibility: ");
-		visibilityLabel.setForeground(Colors.RED_TEXT);
-		visibilityLabel.setFont(Fonts.NORMAL_TEXT);
+		JPanel lethality = createPanel("Lethality: ", _lethality);
 		
 		_visibility = new JProgressBar(0, (int)_disease.getMaxVisibility());
-		_visibility.setValue((int) _disease.getVisibility());
-		_visibility.setBackground(Colors.MENU_BACKGROUND);
-		_visibility.setForeground(Colors.RED_TEXT);
+		JPanel visibility = createPanel("Visibility: ", _visibility);
 		
-		visibility.add(visibilityLabel);
-		visibility.add(_visibility);
+		_heat = new JProgressBar(0, 4);
+		JPanel heat = createPanel("Heat Resistance: ", _heat);
 		
+		_cold = new JProgressBar(0,4);
+		JPanel cold = createPanel("Cold Resistance: ", _cold);
 		
+		_wet = new JProgressBar(0,4);
+		JPanel wet = createPanel("Water Resistance: ", _wet);
 		
-		Utils.setDefaultLook(_infectivity, _lethality, _visibility);
+		_dry = new JProgressBar(0,4);
+		JPanel dry = createPanel("Dry Resistance: ", _dry);
 		
-		add(Box.createRigidArea(new Dimension(0, 25)));
+		_medicine = new JProgressBar(0,4);
+		JPanel medicine = createPanel("Medicine Resistance: ", _medicine);
+		
+		Utils.setDefaultLook(_infectivity, _lethality, _visibility, _heat, _cold, _wet, _dry, _medicine);
+		
+		add(Box.createGlue());
 		add(infectivity);
 		add(lethality);
 		add(visibility);
+		add(heat);
+		add(cold);
+		add(wet);
+		add(dry);
+		add(medicine);
 		add(Box.createGlue());
+	}
+	
+	private JPanel createPanel(String text, JProgressBar pb) {
+		JPanel out = new JPanel();
+		out.setLayout(new BoxLayout(out, BoxLayout.X_AXIS));
+		out.setBackground(Colors.MENU_BACKGROUND);
+		out.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+		
+		JLabel outLabel = new JLabel(text);
+		outLabel.setForeground(Colors.RED_TEXT);
+		outLabel.setFont(Fonts.NORMAL_TEXT);
+		
+		pb.setBackground(Colors.MENU_BACKGROUND);
+		pb.setForeground(Colors.RED_TEXT);
+		
+		out.add(outLabel);
+		out.add(pb);
+		
+		return out;
 	}
 }
