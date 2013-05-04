fcf5c4491a972e69591d452e393ce5fa6c3dd50a
Chet Kilfoyle <Chester_Kilfoyle@brown.edu>
2013-05-04 14:46:43 -0400

    here's the Natty D to use Graham

--- src/edu/brown/cs32/browndemic/region/NaturalDisaster.java
+++ src/edu/brown/cs32/browndemic/region/NaturalDisaster.java
@@ -11,29 +11,24 @@ import java.util.ArrayList;
  * @author ckilfoyl
  */
 public class NaturalDisaster {
-    private double _wealth, _wet, _dry, _heat, _cold, _med;
+    private String _name;
+    private double _wealth, _wet, _dry, _heat, _cold;
 
-    public NaturalDisaster(double wealth, double wet, double dry, double heat,
-            double cold, double med){
+    public NaturalDisaster(String name, double wealth, double wet, double dry, double heat,
+            double cold){
+        _name = name;
         _wealth = wealth;
         _wet = wet;
         _dry = dry;
         _heat = heat;
         _cold = cold;
         _wet = wet;
-        _med = med;
     }
 
+    public String getName(){return _name;}
     public double getWealthFactor(){return _wealth;}
     public double getWetChange(){return _wet;}
     public double getDryChange(){return _dry;}
     public double getHeatChange(){return _heat;}
     public double getColdChange(){return _cold;}
-    public double getMedChange(){return _med;}
-
-    public static ArrayList<NaturalDisaster> buildDisasters(){
-        ArrayList<NaturalDisaster> nattyD = new ArrayList<NaturalDisaster>();
-        //TODO build Nat'l disasters for a region
-        return nattyD;
-    }
 }
