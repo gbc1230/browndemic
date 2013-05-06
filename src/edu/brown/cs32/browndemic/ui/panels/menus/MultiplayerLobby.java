eb8e6ffedeb4cbc4cc9ed0710172fcd69ccea61d
Graham Carling <Graham_Carling@brown.edu>
2013-05-06 16:45:29 -0400

    adding so i can merge

--- src/edu/brown/cs32/browndemic/ui/panels/menus/MultiplayerLobby.java
+++ src/edu/brown/cs32/browndemic/ui/panels/menus/MultiplayerLobby.java
@@ -140,29 +140,24 @@ public class MultiplayerLobby extends UIPanel {
 			JOptionPane.showMessageDialog(this, "The host has disconnected.  Returning to Main Menu");
 			Utils.getParentFrame(this).setPanel(new MainMenu());
 		}
-		try{
-			if (_lobby == null || !_lobby.equals(_thisWorld.getLobby())) {
-				_lobby = _thisWorld.getLobby();
-				_players.removeAll();
-				if (_lobby == null)
-					return;
-				for (LobbyMember l : _lobby) {
-			        _players.add(new MultiplayerLobbyPanel(l.getName(), l.getIP(), _isHost, null, l.isReady()));
-				}
-		        _players.add(Box.createGlue());
-				_players.revalidate();
+		if (_lobby == null || !_lobby.equals(_thisWorld.getLobby())) {
+			_lobby = _thisWorld.getLobby();
+			_players.removeAll();
+			if (_lobby == null)
+				return;
+			for (LobbyMember l : _lobby) {
+			    _players.add(new MultiplayerLobbyPanel(l.getName(), l.getIP(), _isHost, null, l.isReady()));
 			}
-			if (_serverWorld != null) {
-				_start.setEnabled(_serverWorld.allReady());
-			}
-			if (_thisWorld.isGameReady()) {
-				_started = true;
-				Utils.getParentFrame(this).setPanel(new GameMenu(_thisWorld, _thisWorld.getDiseaseID(), true));
-				_timer.stop();
-			}
+		    _players.add(Box.createGlue());
+		    _players.revalidate();
 		}
-		catch(NullPointerException e){
-			System.out.println("Null pointer on: " + _lobby + ", " + _thisWorld);
+		if (_serverWorld != null) {
+			_start.setEnabled(_serverWorld.allReady());
+		}
+		if (_thisWorld.isGameReady()) {
+			_started = true;
+			Utils.getParentFrame(this).setPanel(new GameMenu(_thisWorld, _thisWorld.getDiseaseID(), true));
+			_timer.stop();
 		}
 		if (!_name.equals(Settings.get(Settings.NAME))) {
 			_name = Settings.get(Settings.NAME);
