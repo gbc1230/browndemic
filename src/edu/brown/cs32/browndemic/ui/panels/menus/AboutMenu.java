d7730d7ca4cbcc72f7b89dcd218b8fd80979cc95
Ben Most <bmost1022@gmail.com>
2013-05-08 16:49:48 -0400

    scroll bar on about

--- src/edu/brown/cs32/browndemic/ui/panels/menus/AboutMenu.java
+++ src/edu/brown/cs32/browndemic/ui/panels/menus/AboutMenu.java
@@ -1,10 +1,15 @@
 package edu.brown.cs32.browndemic.ui.panels.menus;
 
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import edu.brown.cs32.browndemic.ui.UIConstants.Colors;
-import edu.brown.cs32.browndemic.ui.UIConstants.Fonts;
 import edu.brown.cs32.browndemic.ui.UIConstants.Strings;
 import edu.brown.cs32.browndemic.ui.UIConstants.UI;
 import edu.brown.cs32.browndemic.ui.Utils;
@@ -13,6 +18,8 @@ import edu.brown.cs32.browndemic.ui.panels.titlebars.BackTitleBar;
 
 public class AboutMenu extends UIPanel {
 	
+	private static final long serialVersionUID = -1483762632451115346L;
+
 	public AboutMenu() {
 		super();
 		makeUI();
@@ -22,12 +29,16 @@ public class AboutMenu extends UIPanel {
 	public void makeUI() {
 		super.makeUI();
 		
-		add(Box.createGlue());
-		System.out.println(Strings.ABOUT_HTML);
 		JLabel about = new JLabel(String.format(Strings.ABOUT_HTML, UI.WIDTH));
 		about.setForeground(Colors.RED_TEXT);
-		add(about);
-		add(Box.createGlue());
+
+		JScrollPane sb = new JScrollPane(about, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		sb.setBackground(Colors.TRANSPARENT);
+		sb.setOpaque(false);
+		sb.getViewport().setBackground(Colors.TRANSPARENT);
+		sb.getViewport().setOpaque(false);
+		sb.setBorder(BorderFactory.createEmptyBorder());
+		add(sb);
 	}
 
 	@Override
