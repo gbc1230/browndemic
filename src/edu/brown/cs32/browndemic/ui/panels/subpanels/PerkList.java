0750bab5a4362eba11b7b7d593e47356280d8df5
Ben Most <bmost1022@gmail.com>
2013-05-08 23:33:48 -0400

    sorting stuff

--- src/edu/brown/cs32/browndemic/ui/panels/subpanels/PerkList.java
+++ src/edu/brown/cs32/browndemic/ui/panels/subpanels/PerkList.java
@@ -41,11 +41,9 @@ public class PerkList extends JPanel implements MouseListener {
 		_buttons.clear();
 		for (Perk p : perks) {
 			Image img = Resources.getImage(Images.PERKS_PATH + p.getImage());
-			System.out.println(p.getImage());
-			PerkPanel pp = new PerkPanel().setImage(img)//Resources.getImage(Images.PERK_LUNGS))
+			PerkPanel pp = new PerkPanel().setImage(img)
 					.setText(p.getName()).setTextFont(Fonts.TITLE_BAR).setTextColor(Colors.RED_TEXT);
 			pp.setBackground(Colors.MENU_BACKGROUND);
-			//pp.setOpaque(false);
 			pp.addMouseListener(this);
 			pp.setBorder(BorderFactory.createEmptyBorder(4, 8, 4, 8));
 			_buttons.put(pp, p);
